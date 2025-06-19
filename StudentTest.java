fc830c4758e1aab64bd7a43eb6a3a4f76ab8d22b
Nollydex <nhvo@cpp.edu>
2025-06-19 13:25:54 -0700

    Update StudentTest.java

--- StudentTest.java
+++ StudentTest.java
@@ -3,8 +3,8 @@ public class StudentTest {
         Student student1 = new Student("John", "CS", 3.5);
 
         Student student2 = new Student();
-        student2.setName("Johnny");
-        student2.setMajor("CS");
+        student2.setName("Mary Ann");
+        student2.setMajor("CE");
         student2.setGpa(3.3);
 
         System.out.println(student1.toString());
